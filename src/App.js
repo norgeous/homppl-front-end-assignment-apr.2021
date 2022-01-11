b92e1153364224787da73dbfcefd05390373bbbd
im-dharte <daniel@harte.me>
2022-01-11 19:08:51 +0000

    feat: add build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 // https://app.swaggerhub.com/apis/Bandsintown/PublicAPI/3.0.0
+// https://rest.bandsintown.com/artists/maroon5/events?app_id=123
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Who’s in town</h1>
+        <small>powered by <a href="https://bandsintown.com">bandsintown.com</a></small>
       </header>
+      interface coming soon!
     </div>
   );
 }
