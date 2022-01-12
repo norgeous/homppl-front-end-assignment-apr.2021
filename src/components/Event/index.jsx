139a5bfa7f6966d2f497c2f18fd5d98b1575a4f3
im-dharte <daniel@harte.me>
2022-01-12 23:03:33 +0000

    feat: date formatter + in x days formatter

--- src/components/Event/index.jsx
+++ src/components/Event/index.jsx
@@ -16,6 +16,34 @@ const getEventName = (eventData, artistData) => {
   return `${name} @ ${venueName}`;
 };
 
+const dFormat = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
+
+const formatDate = (date) => (new Date(date)).toLocaleDateString('en-GB', dFormat);
+
+const intervals = [
+  { label: 'year', seconds: 31536000 },
+  { label: 'month', seconds: 2592000 },
+  { label: 'day', seconds: 86400 },
+  { label: 'hour', seconds: 3600 },
+  { label: 'minute', seconds: 60 },
+  { label: 'second', seconds: 1 },
+];
+
+const relativeTime = (date) => {
+  const seconds = Math.floor((Date.now() - date.getTime()) / 1000);
+  const isPast = seconds > 0;
+  const s = Math.abs(seconds);
+  console.log({ seconds });
+  const interval = intervals.find((i) => i.seconds < s);
+  const count = Math.floor(s / interval.seconds);
+  return `${isPast ? '' : 'in '}${count} ${interval.label}${count !== 1 ? 's' : ''}${isPast ? ' ago' : ''}`;
+};
+
 const Event = ({ eventData, index, artistData }) => {
   if (!eventData) return null;
 
@@ -25,6 +53,8 @@ const Event = ({ eventData, index, artistData }) => {
   } = eventData;
 
   const eventName = getEventName(eventData, artistData);
+  const formattedDatetime = formatDate(datetime);
+  const relative = relativeTime(new Date(datetime));
 
   return (
     <Card sx={{ display: 'flex' }}>
@@ -36,7 +66,7 @@ const Event = ({ eventData, index, artistData }) => {
           {description}
         </Typography>
         <Typography variant="subtitle1" color="text.secondary" component="div">
-          {datetime}
+          {`${formattedDatetime} (${relative})`}
         </Typography>
         {/* <pre>{JSON.stringify(eventData, null, 2)}</pre> */}
       </CardContent>
