f162c019817255084c68779ce1fd10ae5ad79a6d
im-dharte <daniel@harte.me>
2022-01-12 19:49:17 +0000

    feat: add mui menu

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,16 +1,34 @@
-import SearchPage from '../../pages/SearchPage';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-import './App.css';
+import SearchPage from '../../pages/SearchPage';
 
 const App = () => (
   <div className="App">
-    <header className="App-header">
-      <h1>Who’s in town</h1>
-      <small>
-        powered by
-        <a href="https://bandsintown.com">bandsintown.com</a>
-      </small>
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Who’s in town
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
 
     <SearchPage />
   </div>
